1aa95ffc3da674920f44f14365f39c87d8acf145
HASHAN-MD-TEST <Hashanmd2025@gmail.com>
2025-07-17 00:22:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Q4AjUbLQ#18HDoz61VAXpNqAq7vsrZM8gYLAXyK2aYxA6TuBrICg",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94706042889",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
 ALIVE_IMG: process.env.ALIVE_IMG || "Enter The Image URL",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM WHITE MD CREATED BY HASHAN <NOW ALIVE> ",
