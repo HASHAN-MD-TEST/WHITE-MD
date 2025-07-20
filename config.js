c8f598074df604c9b5ac6aaaba309401ff7435f5
HASHAN-MD-TEST <Hashanmd2025@gmail.com>
2025-07-20 09:10:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Q4AjUbLQ#18HDoz61VAXpNqAq7vsrZM8gYLAXyK2aYxA6TuBrICg",
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94706042889",
-AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
-ALIVE_IMG: process.env.ALIVE_IMG || "Enter The Image URL",
-ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM WHITE MD CREATED BY HASHAN <NOW ALIVE> ",
+
+SESSION_ID: process.env.SESSION_ID === undefined ? 'HASHAN-MD=Q4AjUbLQ#18HDoz61VAXpNqAq7vsrZM8gYLAXyK2aYxA6TuBrICg' : process.env.SESSION_ID,
+PREFIX: process.env.PREFIX || '.' ,
+SUDO: process.env.SUDO === undefined ? '94706042889' : process.env.SUDO,
+MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
 };
