b664df8aaa909c4fe2c500885f48e02979515eb0
HASHAN-MD-TEST <Hashanmd2025@gmail.com>
2025-07-21 17:44:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'FwZWzBLI#ZDl1lmNdNlwFPgrWHM1mLMocqu54fHVvNPJvCDSRuEU' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'l0hTWaYT#-yVkzS7B8b6-Tm3PwdRbyR8u6Cr4yHmAWHVrvPGPLZc' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 SUDO: process.env.SUDO === undefined ? '94706042889' : process.env.SUDO,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
